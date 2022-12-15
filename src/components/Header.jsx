7c495bf05e71244b23d5cc72d93c99cd9a4e965a
unknown <agustinpincheira97@gmail.com>
2022-12-15 12:53:52 -0300

    deploy

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="font-black text-5xl mx-auto text-center md:w-2/3">
+        Seguimiento Pacientes {' '}
+        <span className="text-indigo-600">Veterinaria</span>
+        </h1>
+    </header>
+  );
+}
+
+export default Header;
